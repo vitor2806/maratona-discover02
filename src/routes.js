485e5b9a322aeeb7b4033c73f615c5c725874212
Vitor Rafael <vitor.45678.rafael@gmail.com>
2022-01-10 19:43:51 -0300

    change sendFile to render

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const routes = express.Router()
 
+const viewPath = __dirname + '/views/'
+
 //if i'm using only one line function (like this above, where it content will be only a return) I can use it without {} and return
-routes.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
-routes.get('/job', (req, res) => res.sendFile(__dirname + '/views/job.html'))
-routes.get('/job/edit', (req, res) =>
-   res.sendFile(__dirname + '/views/job-edit.html')
-)
-routes.get('/profile', (req, res) =>
-   res.sendFile(__dirname + '/views/profile.html')
-)
+routes.get('/', (req, res) => res.render(viewPath + 'index'))
+routes.get('/job', (req, res) => res.render(viewPath + 'job'))
+routes.get('/job/edit', (req, res) => res.render(viewPath + 'job-edit'))
+routes.get('/profile', (req, res) => res.render(viewPath + 'profile'))
 
 module.exports = routes
