a01827de05e17983abf88b6152cab8538ed857ff
Vitor Rafael <vitor.45678.rafael@gmail.com>
2022-01-10 20:12:28 -0300

    change profile info dinamically with ejs

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,22 @@ const routes = express.Router()
 
 const viewPath = __dirname + '/views/'
 
+const profile = {
+   name: 'Vitor',
+   avatar: 'https://avatars.githubusercontent.com/u/95248045?v=4',
+   'monthly-budget': 4000,
+   'days-per-week': 5,
+   'hours-per-day': 6,
+   'vacations-per-year': 3,
+}
+
 //if i'm using only one line function (like this above, where it content will be only a return) I can use it without {} and return
 routes.get('/', (req, res) => res.render(viewPath + 'index'))
 routes.get('/job', (req, res) => res.render(viewPath + 'job'))
 routes.get('/job/edit', (req, res) => res.render(viewPath + 'job-edit'))
-routes.get('/profile', (req, res) => res.render(viewPath + 'profile'))
+routes.get('/profile', (req, res) =>
+   res.render(viewPath + 'profile', { profile })
+)
 
 module.exports = routes
 //export routes
