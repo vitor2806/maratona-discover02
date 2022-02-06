b1529835d40413ac2a538b16f4c6d2c14f373f0b
Vitor Rafael <vitor.45678.rafael@gmail.com>
2022-02-06 16:57:29 -0300

    add profile value/hour calc

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,54 @@
-const express = require('express');
 //uses express module to configure server
-
-const routes = express.Router();
+const express = require('express');
 //configure file routes through express.Router()
-
+const routes = express.Router();
 const viewPath = __dirname + '/views/';
 
-const profile = {
-   name: 'Vitor',
-   avatar: 'https://avatars.githubusercontent.com/u/95248045?v=4',
-   'monthly-budget': 4000,
-   'days-per-week': 5,
-   'hours-per-day': 6,
-   'vacations-per-year': 3,
-   'amount-hour': 75,
+//object literal
+const Profile = {
+   data: {
+      name: 'Vitor',
+      avatar: 'https://avatars.githubusercontent.com/u/95248045?v=4',
+      'monthly-budget': 4000,
+      'days-per-week': 5,
+      'hours-per-day': 6,
+      'vacation-per-year': 3,
+      'amount-hour': 75,
+   },
+
+   controllers: {
+      index(req, res) {
+         return res.render(viewPath + 'profile', { profile: Profile.data });
+      },
+
+      update(req, res) {
+         //req.body to get data
+         const data = req.body;
+
+         //weeks in a year
+         const weeksInAYear = 52;
+
+         //remove vacation from weeks in a year, getting how much weeks are in a month after it
+         const weeksInAMonth = (weeksInAYear - data['vacation-per-year']) / 12;
+
+         //how much hour a week
+         const weekTotalHours = data['hours-per-day'] * data['days-per-week'];
+
+         //total hours in a month
+         const monthTotalHours = weekTotalHours * weeksInAMonth;
+
+         //amount/hour value
+         const amountPerHour = data['monthly-budget'] / monthTotalHours;
+
+         Profile.data = {
+            ...Profile.data, //first copy all data from profile
+            ...req.body, //then copy all data from req.body
+            'amount-hour': amountPerHour, //then add amountPerHour to 'amount-hour' key
+         };
+
+         return res.redirect('/profile');
+      },
+   },
 };
 
 //object literal
@@ -37,7 +72,7 @@ const Job = {
 
    controllers: {
       index(req, res) {
-         const updatedJobs = jobs.map((job) => {
+         const updatedJobs = Job.data.map((job) => {
             const remaining = Job.tools.remainingDays(job);
             const status = remaining <= 0 ? 'done' : 'progress'; //check if theres any day left, if isnt then its done, if is then its progress
 
@@ -46,11 +81,30 @@ const Job = {
                ...job,
                remaining,
                status,
-               budget: profile['amount-hour'] * job['total-hours'], //if i'm working 10 hours in a project and my amount-hour is R$ 10,00, i'll get R$ 100,00
+               budget: Profile.data['amount-hour'] * job['total-hours'], //if i'm working 10 hours in a project and my amount-hour is R$ 10,00, i'll get R$ 100,00
             };
          });
 
          return res.render(viewPath + 'index', { jobs: updatedJobs });
+      },
+
+      create(req, res) {
+         return res.render(viewPath + 'job');
+      },
+
+      save(req, res) {
+         const lastElementId = Job.data[Job.data.length - 1]?.id || 1; //will search id from jobs if theres no element with id, id will be 1.
+
+         //Will push a new job into jobs array with req.body data
+         Job.data.push({
+            id: lastElementId + 1,
+            name: req.body.name,
+            'daily-hours': req.body['daily-hours'],
+            'total-hours': req.body['total-hours'],
+            createdAt: Date.now(),
+         });
+
+         return res.redirect('/');
       },
    },
 
@@ -76,27 +130,13 @@ const Job = {
 //map vs foreach
 //I can store map returnal into a variable
 //I cant store forEach into it
+
 routes.get('/', Job.controllers.index);
-
-routes.get('/job', (req, res) => res.render(viewPath + 'job'));
-//Routes.post will handle post form method
-routes.post('/job', (req, res) => {
-   const lastElementId = jobs[jobs.length - 1]?.id || 1; //will search id from jobs if theres no element with id, id will be 1.
-
-   //Will push a new job into jobs array with req.body data
-   jobs.push({
-      id: lastElementId + 1,
-      name: req.body.name,
-      'daily-hours': req.body['daily-hours'],
-      'total-hours': req.body['total-hours'],
-      createdAt: Date.now(),
-   });
-   return res.redirect('/');
-});
+routes.get('/job', Job.controllers.create);
+routes.post('/job', Job.controllers.save); //Routes.post will handle post form method
 routes.get('/job/edit', (req, res) => res.render(viewPath + 'job-edit'));
-routes.get('/profile', (req, res) =>
-   res.render(viewPath + 'profile', { profile })
-);
+routes.get('/profile', Profile.controllers.index);
+routes.post('/profile', Profile.controllers.update);
 
 //export routes
 module.exports = routes;
