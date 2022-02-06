29e0ea79bfe1bb233b65174e0e6ef2906f238430
Vitor Rafael <vitor.45678.rafael@gmail.com>
2022-02-06 16:15:46 -0300

    extract jobs array to Job data extract remainingDays function to Job tools

--- src/routes.js
+++ src/routes.js
@@ -18,10 +18,27 @@ const profile = {
 
 //object literal
 const Job = {
+   data: [
+      {
+         id: 1,
+         name: 'Pizzaria Guloso',
+         'daily-hours': 2,
+         'total-hours': 1,
+         createdAt: Date.now(),
+      },
+      {
+         id: 2,
+         name: 'OneTwo Project',
+         'daily-hours': 3,
+         'total-hours': 40,
+         createdAt: Date.now(),
+      },
+   ],
+
    controllers: {
       index(req, res) {
          const updatedJobs = jobs.map((job) => {
-            const remaining = remainingDays(job);
+            const remaining = Job.tools.remainingDays(job);
             const status = remaining <= 0 ? 'done' : 'progress'; //check if theres any day left, if isnt then its done, if is then its progress
 
             //js object spread
@@ -36,38 +53,25 @@ const Job = {
          return res.render(viewPath + 'index', { jobs: updatedJobs });
       },
    },
+
+   tools: {
+      remainingDays(job) {
+         const dayRemaining = (
+            job['total-hours'] / job['daily-hours']
+         ).toFixed(); //This will get, i.e.: 40/2 = 20 days to complete
+         const createdAt = new Date(job.createdAt); //this will get the DAY that project was started, i.e.: 01/01
+         const dueDay = createdAt.getDate() + Number(dayRemaining); //this will get the due day, i.e.: if it was started at 01/01 and I will work 40 hours but 2 by day, 21/01 will be my due day
+         const dueDate = createdAt.setDate(dueDay);
+
+         const timeDiff = dueDate - Date.now();
+         // 86400000 = 1 day in ms
+         const dayMs = 24 * 60 * 60 * 1000;
+         const dayDiff = Math.floor(timeDiff / dayMs); //remaining days until last day
+
+         return dayDiff;
+      },
+   },
 };
-
-const jobs = [
-   {
-      id: 1,
-      name: 'Pizzaria Guloso',
-      'daily-hours': 2,
-      'total-hours': 1,
-      createdAt: Date.now(),
-   },
-   {
-      id: 2,
-      name: 'OneTwo Project',
-      'daily-hours': 3,
-      'total-hours': 40,
-      createdAt: Date.now(),
-   },
-];
-
-function remainingDays(job) {
-   const dayRemaining = (job['total-hours'] / job['daily-hours']).toFixed(); //This will get, i.e.: 40/2 = 20 days to complete
-   const createdAt = new Date(job.createdAt); //this will get the DAY that project was started, i.e.: 01/01
-   const dueDay = createdAt.getDate() + Number(dayRemaining); //this will get the due day, i.e.: if it was started at 01/01 and I will work 40 hours but 2 by day, 21/01 will be my due day
-   const dueDate = createdAt.setDate(dueDay);
-
-   const timeDiff = dueDate - Date.now();
-   // 86400000 = 1 day in ms
-   const dayMs = 24 * 60 * 60 * 1000;
-   const dayDiff = Math.floor(timeDiff / dayMs); //remaining days until last day
-
-   return dayDiff;
-}
 
 //map vs foreach
 //I can store map returnal into a variable
